25248a636bb600f71a8099a6f6c95ddb78ae14e9
Himanshu Borikar <himanshuborikar10@gmail.com>
2025-07-01 21:02:43 +0530

    Data Transformation Done

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,21 @@
 # any general function that is used in the entire programe is written here
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+import dill
+
+from src.execption import CustomException
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
