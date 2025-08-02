d567482075a63f0ae8f9c0d063baf52f58e5330c
Himanshu Borikar <himanshuborikar10@gmail.com>
2025-08-02 19:48:24 +0530

    Fix: Changed model path to be OS-independent

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.execption import CustomException
 from src.utils import load_object
@@ -10,7 +11,7 @@ class PredictPipeline:
     
     def predict(self,features):
         try:
-            model_path='artifacts\model.pkl'
+            model_path = os.path.join('artifacts', 'model.pkl')
             preprocessor_path='artifacts\preprocessor.pkl'
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
